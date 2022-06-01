0ed5f0b3a8cb26df5c36e382182d58abf6418963
selinarslanbulut <arslanbulutselin@outlook.com>
2022-06-01 10:34:42 +0300

    Subject added

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -7,14 +8,17 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit , OnDestroy {
 
   ingredients!: Ingredient[];
+  private igChangeSub!: Subscription;
+
+
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit(){
     this.ingredients=this.slService.getIngredients();
-    this.slService.ingredientsChanged.subscribe(
+    this.igChangeSub =this.slService.ingredientsChanged.subscribe(
       (ingredients: Ingredient[])=>{
         this.ingredients=ingredients;
       }
@@ -22,4 +26,7 @@ export class ShoppingListComponent implements OnInit {
     );
   }
 
+  ngOnDestroy(): void {
+    this.igChangeSub.unsubscribe();
+  }
 }
