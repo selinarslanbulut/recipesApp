30001c671991036a6ee7b8d3270466c0d43e762a
selinarslanbulut <arslanbulutselin@outlook.com>
2022-06-15 10:03:12 +0300

    edit-list complated

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -15,7 +15,8 @@ export class RecipeEditComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private recipeService : RecipeService
+    private recipeService : RecipeService,
+    private router : Router
     ) { }
   
   ngOnInit() {
@@ -30,7 +31,18 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onSubmit(){
-    console.log(this.recipeForm);
+    // const newRecipe = new Recipe(
+    // this.recipeForm.value['name'], 
+    // this.recipeForm.value['description'],
+    // this.recipeForm.value['imagePath'],
+    // this.recipeForm.value['ingredients']
+    // );
+    if(this.editMode){
+      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+    } else{
+      this.recipeService.addRecipe(this.recipeForm.value);
+    }
+    this.onCancel();
   }
 
   onAddIngredient(){
@@ -43,7 +55,12 @@ export class RecipeEditComponent implements OnInit {
         ])
       })
     )
-
+  }
+  onDeleteIngredient(index: number){
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+  onCancel(){
+    this.router.navigate(['../'], {relativeTo: this.route});
   }
 
 //formu başlatma
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component,OnInit} from '@angular/core';
+import { Component,OnDestroy,OnInit} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Recipe} from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -9,9 +10,10 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-list.component.css']
 })
 
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
 
   recipes!: Recipe[];
+  subscription!: Subscription;
   
   constructor(
     private RecipeService: RecipeService,
@@ -20,12 +22,21 @@ export class RecipeListComponent implements OnInit {
     ) { }
 
   ngOnInit(){
+    this.subscription = this.RecipeService.recipesChanged
+    .subscribe(
+      (recipes: Recipe[]) =>{
+        this.recipes = recipes;
+      }
+    )
     this.recipes=this.RecipeService.getRecipes();
   }
 
   onNewRecipe(){
     this.router.navigate(['new'], {relativeTo:this.route});
+  }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
