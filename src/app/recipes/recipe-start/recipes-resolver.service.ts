124406d2f26d696fe00ee21a227d5a4f47cddfa7
selinarslanbulut <arslanbulutselin@outlook.com>
2022-06-21 10:41:47 +0300

    updated

---
+++ src/app/recipes/recipe-start/recipes-resolver.service.ts
@@ -0,0 +1,18 @@
+// import { Injectable } from '@angular/core';
+// import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+// import { Observable } from 'rxjs';
+// import { DataStorageService } from 'src/app/shared/data-storage.service';
+// import { Recipe } from '../recipe.model';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// // export class RecipesResolverService implements Resolve<Recipe[]> {
+
+// //   constructor(private dataStorageService: DataStorageService) { }
+
+// //   // resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+    
+// //   // }
+// //}
+ 
